2b91be73204130baedd5a08a6de564e30edcf881
Jonathan Silveira <jonathansouzadasilveira17@gmail.com>
2025-01-15 09:35:25 -0300

    adjusts

--- cypress.config.js
+++ cypress.config.js
@@ -6,11 +6,5 @@ module.exports = defineConfig({
 		defaultCommandTimeout: 10000,
 		viewportWidth: 1280,
 		viewportHeight: 720,
-		baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
-		env: {
-			apiUrl: process.env.CYPRESS_API_URL,
-			username: process.env.CYPRESS_USERNAME,
-			password: process.env.CYPRESS_PASSWORD
-		}
 	},
 });
--- run-hit-little-point.sh
+++ run-hit-little-point.sh
@@ -7,7 +7,7 @@ cd /Users/jonathansilveira/Documents/hitpoint
 echo "Starting Cypress test at $(date)" >> cypress-cron.log
 
 # Run the test and log output
-npm run test:hitPoint >> cypress-cron.log 2>&1
+npm run test:hitpoint >> cypress-cron.log 2>&1
 
 # Log the end time
 echo "Finished at $(date)" >> cypress-cron.log
